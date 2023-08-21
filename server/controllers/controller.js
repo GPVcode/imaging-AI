1d32bf839aa64187ad4993b64090d714d73a9fe0
GPV <gvill005@gmail.com>
2023-08-21 15:04:37 -0700

    adding console logs for debugging

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,10 +1,12 @@
 import fetch from 'node-fetch';
 
 const generatePhotos = async (inputValue) => {
+    console.log("touch test")
     const openaiUrl = "https://api.openai.com/v1/images/generations";
     const apiKey = process.env.Openai_Access_Key;
     const bearer = 'Bearer ' + apiKey;
     try {
+        console.log("touch test in try block")
         const response = await fetch(openaiUrl, {
             method: 'POST',
             headers: {
@@ -34,8 +36,10 @@ const generatePhotos = async (inputValue) => {
 
 
 const getPhotos = async (inputValue, page) => {
+    console.log("touch test")
     const accessKey =  process.env.Unsplash_Access_Key
     try{
+        console.log("touch test in try block")
         const response = await fetch(`https://api.unsplash.com/search/photos?page=${page}&query=${inputValue}&client_id=${accessKey}`)
         const data = await response.json();
         const results = data.results;
